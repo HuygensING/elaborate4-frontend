a877f088d0dcdcff92b462884f137bb9f4963b7a
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2023-09-12 13:45:47 +0200

    set version to 2.0.7

--- docker/publication/cmd.sh
+++ docker/publication/cmd.sh
@@ -5,5 +5,7 @@ echo && \
 echo "> testing /nginx_conf/nginx.conf:" && \
 nginx -t -c /nginx_conf/nginx.conf && \
 echo && \
+sed -e "s|BASE_URL = ''|BASE_URL = '$BASE_URL'|" /usr/share/nginx/html/index.html | sed -e "s|\"/static/|\"$BASE_URL/static/|" | sed -e "s|\"/js/|\"$BASE_URL/js/|" > /usr/share/nginx/html/index.html.bak && \
+cp /usr/share/nginx/html/index.html.bak /usr/share/nginx/html/index.html && \
 echo "> starting nginx:" && \
 nginx -c /nginx_conf/nginx.conf -g 'daemon off;'
